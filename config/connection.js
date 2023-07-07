73fe0b1f8298e4d248e6bf55e95c34d5be4b8bc4
wumengchao1989 <332982129@qq.com>
2023-07-07 15:04:12 +0800

    remove azure secret

--- config/connection.js
+++ config/connection.js
@@ -3,41 +3,26 @@ const mongoose = require("mongoose");
 const { getSecret } = require("./keyvault");
 
 async function putKeyVaultSecretInEnvVar() {
+  const secretName = process.env.KEY_VAULT_SECRET_NAME_DATABASE_URL;
+  const keyVaultName = process.env.KEY_VAULT_NAME;
 
-    const secretName = process.env.KEY_VAULT_SECRET_NAME_DATABASE_URL;
-    const keyVaultName = process.env.KEY_VAULT_NAME;
+  console.log(secretName);
+  console.log(keyVaultName);
 
-    console.log(secretName);
-    console.log(keyVaultName);
-    
-    if (!secretName || !keyVaultName) throw Error("getSecret: Required params missing");
+  if (!secretName || !keyVaultName)
+    throw Error("getSecret: Required params missing");
 
-    connectionString = await getSecret(secretName, keyVaultName);
-    process.env.DATABASE_URL = connectionString;
-
+  connectionString = await getSecret(secretName, keyVaultName);
+  process.env.DATABASE_URL = connectionString;
 }
 
 async function getConnectionInfo() {
-  if (!process.env.DATABASE_URL) {
-
-    await putKeyVaultSecretInEnvVar();
-
-    // still don't have a database url?
-    if(!process.env.DATABASE_URL){
-      throw new Error("No value in DATABASE_URL in env var");
-    }
-  }
-
-  // To override the database name, set the DATABASE_NAME environment variable in the .env file
-  const DATABASE_NAME = process.env.DATABASE_NAME || "azure-todo-app";
-  
   return {
     DATABASE_URL: process.env.DATABASE_URL,
-    DATABASE_NAME: process.env.DATABASE_NAME
-  }
+    DATABASE_NAME: process.env.DATABASE_NAME,
+  };
 }
-
 
 module.exports = {
-  getConnectionInfo
-}
+  getConnectionInfo,
+};
