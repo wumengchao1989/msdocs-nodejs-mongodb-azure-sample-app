4fd1693f601ff51082dc061cb86262a18a980893
LAPTOP-P7G9LM4M\wumen <332982129@qq.com>
2023-07-08 14:21:53 +0800

    upd：env

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
-console.log(process.env.NODE_ENV)
 
 async function getConnectionInfo() {
-  console.log(process.env);
-  console.log('markname',process.env.markname)
   return {
-    DATABASE_URL: process.env.AZURE_COSMOS_CONNECTIONSTRING,
+    DATABASE_URL: process.env.markname==='wmc'?process.env.DATABASE_URL:process.env.DATABASE_DEV_URL,
     DATABASE_NAME: process.env.DATABASE_NAME,
   };
 }
