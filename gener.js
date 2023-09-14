89edb81dfac7f56452505a7eeb7b062998e750f4
LAPTOP-P7G9LM4M\wumen <332982129@qq.com>
2023-09-14 18:18:29 +0800

    udp

--- gener.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const tsFile = path.resolve("./api/angulardemo.ts");
-const resFile = path.resolve("./api/result.ts");
-let file = fs.readFileSync(tsFile).toString();
-file = file.replace(/\\n/g, " ");
-file = file.replace(/\\n/g, " ");
-fs.writeFileSync(resFile, file);
